6b4e2cef9fa91ad1d3324045482b92d2630eed0c
Rafael Becerra <rafaelbecks93@gmail.com>
2021-08-10 20:57:51 +0200

    add menu screen, use flick gesture instead of doubletap

--- firmware.py
+++ firmware.py
@@ -31,7 +31,7 @@ def splash_screen(screen):
         draw.text((23, 11), text="\ue902", font=icon, fill="white")
         draw.text((5, 100), text="gesture midi controller   ", font=title, fill="white")
         draw.text((55, 112), text="v0.1   ", font=title, fill="white")
-    time.sleep(3)
+    time.sleep(2)
 
 def midi_device_selection(screen, **kwargs): 
     kwargs["selected"] = 0
@@ -42,17 +42,17 @@ def midi_device_selection(screen, **kwargs):
     real_midi_outputs = filter(lambda x: 'Midi Through:' not in x,mido.get_output_names())
     midi_devices = list(set(real_midi_outputs))
     
-    @skywriter.touch(selected)
+    @skywriter.touch(2)
     def touch(position):
-        print(kwargs["selected"] )
         if(kwargs["selected"]  != len(midi_devices) -1):
             kwargs["selected"]  += 1
         else:
             kwargs["selected"] = 0
 
-    @skywriter.double_tap()
-    def doubletap(position):
-     kwargs['device-selected'] = True
+    @skywriter.flick()
+    def select(start,finnish):
+        if(finnish == 'east'):
+            kwargs['device-selected'] = True
   
     while kwargs['device-selected'] == False:
         with canvas(screen) as draw:
@@ -70,13 +70,41 @@ def midi_device_selection(screen, **kwargs):
                 
                 
 def main_menu(screen, **kwargs):
+    kwargs["selected"] = 0
     icon = make_font("icomoon.ttf", 45)
     random_icon = make_font("icomoon.ttf", 58)
-    while True:
+    menu_item = make_font('bandeins.ttf', 12)
+    kwargs['screen-active'] = True
+    options = ['chords', 'arp', 'random']
+    
+    position = {
+        'chords': (12,55,53,65),
+        'arp': (86,55,107,66),
+        'random': (44,119,87,128)
+    }
+
+    @skywriter.touch(2)
+    def touch(position):
+        if(kwargs["selected"]  != 2):
+            kwargs["selected"]  += 1
+        else:
+            kwargs["selected"] = 0
+    
+    def color_selection(selection):
+        if(kwargs['selected'] == selection):
+            return 'black'
+        else:   
+            return 'white'
+                      
+    while kwargs['screen-active']:
         with canvas(screen) as draw:
-            draw.text((11, 9), text="\ue903", font=icon, fill="white")
-            draw.text((75, 9), text="\ue905", font=icon, fill="white")
-            draw.text((36, 65), text="\ue904", font=random_icon, fill="white")
+            draw.rectangle(position[options[kwargs['selected']]], fill='white')
+            draw.text((11, 5), text="\ue903", font=icon, fill='white' )
+            draw.text((13, 53), text="chords  ", font=menu_item, fill=color_selection(0))
+            draw.text((75, 5), text="\ue905", font=icon, fill="white")
+            draw.text((87, 53), text="arp  ", font=menu_item, fill=color_selection(1))
+            draw.text((36, 62), text="\ue904", font=random_icon, fill="white")
+            draw.text((44, 116), text="random  ", font=menu_item, fill=color_selection(2))
 
 
 if __name__ == "__main__":
