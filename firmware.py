b99e47a0845c27428d7ba9e00e92ffbc26ae6842
Rafael Becerra <rafaelbecks93@gmail.com>
2021-08-13 21:12:29 +0200

    navigation, organized code and linting

--- firmware.py
+++ firmware.py
@@ -4,115 +4,131 @@
 import skywriter
 import signal
 
-from pathlib import Path
-from PIL import ImageFont
 
 from screen_device import get_device
+from ui import icons, make_font, menu_position, color_selection, splash_screen
 from luma.core.render import canvas
-import time
 
 import mido
 
-
-codes = [
-    "\ue900", "\ue901", "\ue902", "\ue903", "\ue904", "\ue905"
-]
-
-def make_font(name, size):
-    font_path = str(Path(__file__).resolve().parent.joinpath('fonts', name))
-    return ImageFont.truetype(font_path, size)
-
-
-def splash_screen(screen):
-    icon = make_font("icomoon.ttf", 80)
-    title = make_font('bandeins.ttf', 11)
-
-    with canvas(screen) as draw:
-        draw.text((23, 11), text="\ue902", font=icon, fill="white")
-        draw.text((5, 100), text="gesture midi controller   ", font=title, fill="white")
-        draw.text((55, 112), text="v0.1   ", font=title, fill="white")
-    time.sleep(2)
-
-def midi_device_selection(screen, **kwargs): 
+def midi_device_selection(screen, **kwargs):
     kwargs["selected"] = 0
     kwargs['device-selected'] = False
-    selected = 0   
+    selected = 0
     icon = make_font("icomoon.ttf", 64)
     selection = make_font('bandeins.ttf', 11)
-    real_midi_outputs = filter(lambda x: 'Midi Through:' not in x,mido.get_output_names())
+    real_midi_outputs = filter(
+        lambda x: 'Midi Through:' not in x, mido.get_output_names())
     midi_devices = list(set(real_midi_outputs))
-    
+
     @skywriter.touch(2)
     def touch(position):
-        if(kwargs["selected"]  != len(midi_devices) -1):
-            kwargs["selected"]  += 1
+        if(kwargs["selected"] != len(midi_devices) - 1):
+            kwargs["selected"] += 1
         else:
             kwargs["selected"] = 0
 
     @skywriter.flick()
-    def select(start,finnish):
+    def select(start, finnish):
         if(finnish == 'east'):
             kwargs['device-selected'] = True
-  
+
     while kwargs['device-selected'] == False:
         with canvas(screen) as draw:
-            draw.text((32, 3), text="\ue900", font=icon, fill="white")
-            draw.rectangle((0,80 + (16 * kwargs["selected"]),128,65 + (16 * kwargs["selected"])), fill='white',width=128)
-            
-                    
+            draw.text((32, 3), text=icons['device'], font=icon, fill="white")
+            draw.rectangle((0, 80 + (16 * kwargs["selected"]), 128, 65 + (
+                16 * kwargs["selected"])), fill='white', width=128)
+
             for index, item in enumerate(midi_devices):
                 if(index == kwargs["selected"]):
                     color = 'black'
                 else:
                     color = 'white'
 
-                draw.text((10, 66 + (16 * index)), text=item[:item.index(':')] + '  ', font=selection, fill=color, stroke_fill='red')
-                
-                
-def main_menu(screen, **kwargs):
-    kwargs["selected"] = 0
-    icon = make_font("icomoon.ttf", 45)
+                draw.text((10, 66 + (16 * index)), text=item[:item.index(
+                    ':')] + '  ', font=selection, fill=color, stroke_fill='red')
+
+    return midi_devices[kwargs["selected"]]
+
+
+def main_menu(screen, midi_device, **kwargs):
+    menu_icon = make_font("icomoon.ttf", 45)
     random_icon = make_font("icomoon.ttf", 58)
     menu_item = make_font('bandeins.ttf', 12)
-    kwargs['screen-active'] = True
     options = ['chords', 'arp', 'random']
-    
-    position = {
-        'chords': (12,55,53,65),
-        'arp': (86,55,107,66),
-        'random': (44,119,87,128)
-    }
+    kwargs["selected"] = 0
+    kwargs['selection_made'] = False
 
+    @skywriter.flick()
+    def select(start, finnish):
+        if(finnish == 'east'):
+             kwargs['selection_made'] = True
+              
     @skywriter.touch(2)
     def touch(position):
-        if(kwargs["selected"]  != 2):
-            kwargs["selected"]  += 1
+        if(kwargs["selected"] != 2):
+            kwargs["selected"] += 1
         else:
             kwargs["selected"] = 0
-    
-    def color_selection(selection):
-        if(kwargs['selected'] == selection):
-            return 'black'
-        else:   
-            return 'white'
-                      
-    while kwargs['screen-active']:
-        with canvas(screen) as draw:
-            draw.rectangle(position[options[kwargs['selected']]], fill='white')
-            draw.text((11, 5), text="\ue903", font=icon, fill='white' )
-            draw.text((13, 53), text="chords  ", font=menu_item, fill=color_selection(0))
-            draw.text((75, 5), text="\ue905", font=icon, fill="white")
-            draw.text((87, 53), text="arp  ", font=menu_item, fill=color_selection(1))
-            draw.text((36, 62), text="\ue904", font=random_icon, fill="white")
-            draw.text((44, 116), text="random  ", font=menu_item, fill=color_selection(2))
 
+    while  kwargs['selection_made'] != True:
+        with canvas(screen) as draw:
+            draw.rectangle(
+                menu_position[options[kwargs['selected']]], fill='white')
+            draw.text((11, 5), text=icons['chords'],
+                      font=menu_icon, fill='white')
+            draw.text((13, 53), text="chords  ",
+                      font=menu_item, fill=color_selection(0, kwargs['selected']))
+            draw.text((75, 5), text=icons['arp'], font=menu_icon, fill="white")
+            draw.text((87, 53), text="arp  ", font=menu_item,
+                      fill=color_selection(1, kwargs['selected']))
+            draw.text((36, 62), text=icons['random'],
+                      font=random_icon, fill="white")
+            draw.text((44, 116), text="random  ",
+                      font=menu_item, fill=color_selection(2, kwargs['selected']))
+
+    return kwargs['selected']
+
+
+def chord(screen, midi_device, **kwargs):
+    icon = make_font("icomoon.ttf", 80)
+    kwargs['screen_active'] = True
+    print(midi_device)
+
+    @skywriter.flick()
+    def select(start, finnish):
+        print(finnish)
+        if(finnish == 'west'):
+          kwargs['screen_active'] = False
+            
+    while kwargs['screen_active']:
+      with canvas(screen) as draw:
+          draw.text((23, 11), text=icons['chords'], font=icon, fill="white")
+
+
+modules = {
+  0: chord,
+}
+
+def boot(**kwargs):
+    screen = get_device()
+    splash_screen(screen)
+
+
+    midi_device = midi_device_selection(screen)
+
+    while True:
+      selection = main_menu(screen, midi_device)
+      print(selection)
+      if(selection >= 0):
+        modules[selection](screen, midi_device)   
+      selection == -1
+      print('end flow')     
+    
 
 if __name__ == "__main__":
     try:
-        screen = get_device()
-        splash_screen(screen)
-        midi_device_selection(screen)
-        main_menu(screen)
+        boot()
     except KeyboardInterrupt:
         pass
 
---
+++ ui.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from PIL import ImageFont
+from pathlib import Path
+import time
+from luma.core.render import canvas
+
+icons = {
+    'bird': "\ue902",
+    'device': "\ue900",
+    'chords': "\ue903",
+    'arp': "\ue905",
+    "random": "\ue904"
+
+}
+
+
+def make_font(name, size):
+    font_path = str(Path(__file__).resolve().parent.joinpath('fonts', name))
+    return ImageFont.truetype(font_path, size)
+
+
+menu_position = {
+    'chords': (12, 55, 53, 65),
+    'arp': (86, 55, 107, 66),
+    'random': (44, 119, 87, 128)
+}
+
+
+def color_selection(selection, selected):
+    if(selected == selection):
+        return 'black'
+    else:
+        return 'white'
+
+
+def splash_screen(screen):
+    icon = make_font("icomoon.ttf", 80)
+    title = make_font('bandeins.ttf', 11)
+
+    with canvas(screen) as draw:
+        draw.text((23, 11), text=icons['bird'], font=icon, fill="white")
+        draw.text((5, 100), text="gesture midi controller   ",
+                  font=title, fill="white")
+        draw.text((55, 112), text="v0.1   ", font=title, fill="white")
+    time.sleep(2)
